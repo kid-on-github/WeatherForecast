80228216426e5dca8f30ffb207a0e9cff8a5802e
kid-on-github <mike@lemonshell.com>
2021-05-13 11:13:35 -0700

    Updates

---
+++ precache-manifest.309590bc47fa9b29c1425d3f33e070a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "013d12666d0f5453943826a7d9f06e5a",
+    "url": "/WeatherForecast/index.html"
+  },
+  {
+    "revision": "e687ed3ccfc3e447195a",
+    "url": "/WeatherForecast/static/css/main.16d19f90.chunk.css"
+  },
+  {
+    "revision": "f1887c6a7aa24ab645e6",
+    "url": "/WeatherForecast/static/js/2.2aa374e8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WeatherForecast/static/js/2.2aa374e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e687ed3ccfc3e447195a",
+    "url": "/WeatherForecast/static/js/main.f99cb6cc.chunk.js"
+  },
+  {
+    "revision": "204e1d4645009158b699",
+    "url": "/WeatherForecast/static/js/runtime-main.953e2e29.js"
+  }
+]);
